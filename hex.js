cd173f7d7cf01b985e67b090b00419ab774d01a7
vibhakotian <88483094+vibhakotian@users.noreply.github.com>
2023-09-24 14:24:12 +0530

    Add files via upload

---
+++ hex.js
@@ -0,0 +1,18 @@
+const colors=[0,1,2,3,4,5,6,7,8,"A","B","C","D","E","F"];
+
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+btn.addEventListener("click",function(){
+    let hexcolor="#";
+    for(let i=0;i<6;i++)
+    {
+        hexcolor+=colors[getRandomColor()];
+    }
+const randomnumber=getRandomColor();
+document.body.style.backgroundColor=hexcolor;
+color.textContent=hexcolor;
+});
+function getRandomColor()
+{
+    return Math.floor(Math.random()*colors.length);
+}
